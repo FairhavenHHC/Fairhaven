d4ed071daadf94509c365f586448d154fac87d59
sindiazi <saindiazi@gmail.com>
2016-02-03 19:31:25 -0600

    Build on 2-3-2016

--- target/generated-sources/annotations/com/fairhaven/db/entities/Groups_.java
+++ target/generated-sources/annotations/com/fairhaven/db/entities/Groups_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-02-02T19:17:32")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-02-03T19:27:27")
 @StaticMetamodel(Groups.class)
 public class Groups_ { 
 
