95a59b4c611d0a9d5bf0a7aa97abecde2e4bb717
sindiazi <saindiazi@gmail.com>
2016-02-15 08:14:06 -0600

    New clone in Samsteel D drive after crash

--- target/generated-sources/annotations/com/fairhaven/db/entities/Groups_.java
+++ target/generated-sources/annotations/com/fairhaven/db/entities/Groups_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-02-07T00:44:33")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-02-14T15:55:13")
 @StaticMetamodel(Groups.class)
 public class Groups_ { 
 
