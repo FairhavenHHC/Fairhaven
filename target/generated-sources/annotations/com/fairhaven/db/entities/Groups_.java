fe152ed818883e8be02cf82b2503c2c0987ddf07
sindiazi <saindiazi@gmail.com>
2016-05-05 08:13:12 -0500

    Cleaning

--- target/generated-sources/annotations/com/fairhaven/db/entities/Groups_.java
+++ target/generated-sources/annotations/com/fairhaven/db/entities/Groups_.java
@@ -6,23 +6,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-04T23:00:44")
-=======
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-04-19T20:53:29")
-=======
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-03-28T22:17:53")
-=======
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-02-14T15:55:13")
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-03-26T16:49:44")
->>>>>>> login
->>>>>>> c4c55a6de9ef3c9716c8d750a5b4d5d57a1445b2
->>>>>>> 2bf9b035f0db61dbe80228b054532731c6f55b2d
->>>>>>> master
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-05T08:11:44")
 @StaticMetamodel(Groups.class)
 public class Groups_ { 
 
