1d554b21e6f6c8617dfa92304a412ceb17d1e6f7
sindiazi <saindiazi@gmail.com>
2016-05-05 08:10:55 -0500

    Fixed some conflicts

--- src/main/java/com/fairhaven/web/controllers/IndexController.java
+++ src/main/java/com/fairhaven/web/controllers/IndexController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -53,49 +52,3 @@ public class IndexController {
     }
 
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.fairhaven.web.controllers;
-
-// Import log4j class
-import com.fairhaven.db.dao.DAOFactory;
-import java.util.Locale;
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import org.apache.log4j.Logger;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
-/**
- *
- * @author Sam
- */
-@Controller
-public class IndexController {
-
-    private static final Logger logger = Logger.getLogger(IndexController.class.getName());
-
-    @Resource
-    private DAOFactory daof;
-
-    /**
-     *
-     * @param locale
-     * @param request
-     * @return
-     */
-    @RequestMapping(value = "/index.htm", method = RequestMethod.GET)
-    public ModelAndView index(Locale locale, HttpServletRequest request) {
-        ModelAndView mav = new ModelAndView("com.fairhaven.index");
-        mav.addObject("services", daof.getServicesDAO().findAll());
-        return mav;
-    }
-
-}
->>>>>>> master
